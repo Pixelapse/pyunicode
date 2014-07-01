55b55b1e34eaf30969484c29638c30d675b6d306
Shravan Reddy <shravan@cs.stanford.edu>
2014-06-30 20:35:10 -0700

    define corresponding safely_encode

--- pyunicode/api.py
+++ pyunicode/api.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def safely_decode(unicode_or_str, encoding='utf-8'):
-  ''' Decodes <str> into <unicode>. Ignores any non-utf8 chars in <str>s '''
+  ''' Decodes byte <str> into <unicode>. Ignores any non-utf8 chars in <str>s '''
   if isinstance(unicode_or_str, unicode):
     ustr = unicode_or_str
   elif isinstance(unicode_or_str, str):
@@ -20,3 +20,15 @@ def safely_decode(unicode_or_str, encoding='utf-8'):
     raise Exception(u'Not of type unicode or str')
 
   return ustr
+
+
+def safely_encode(unicode_or_str, encoding='utf-8'):
+  ''' Encodes <unicode> into byte <str>. Replaces any non utf8 chars '''
+  if isinstance(unicode_or_str, unicode):
+    rstr = unicode_or_str.encode(encoding, 'replace')
+  elif isinstance(unicode_or_str, str):
+    rstr = unicode_or_str
+  else:
+    raise Exception(u'Not of type unicode or str')
+
+  return rstr
